72ea9f655001d44aa6e660df0f536421ddbae298
Mark Tareshawty <mark@marktareshawty.com>
2015-03-26 21:08:12 -0400

    use sortBy and reorder model as they come in

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return this.store.findAll('friend');
+  queryParams: {
+    sortBy: {
+      refreshModel: true
+    },
+
+    sortAscending: {
+      refreshModel: true
+    }
+  },
+
+  model: function(params) {
+    return this.store.findQuery('friend', params);
   },
 });
