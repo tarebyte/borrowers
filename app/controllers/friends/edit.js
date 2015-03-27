846aff8b858c274ce52b0f00fc9f9fb418cfda60
Mark Tareshawty <mark@marktareshawty.com>
2015-03-26 21:49:34 -0400

    redirect to articles index instead of friends show

--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -3,7 +3,9 @@ import FriendsBaseController from './base';
 export default FriendsBaseController.extend({
   actions: {
     cancel: function() {
-      this.transitionToRoute('friends.show', this.get('model'));
+      // Go back to the articles page so that you get the
+      // whole page
+      this.transitionToRoute('articles.index', this.get('model'));
       return false;
     }
   }
